67852cc8bec69742a7785dfe28cdc1f063cbf300
vineet0810 <vineet.kulshrestha@justclickkaro.com>
2019-03-15 13:56:56 +0530

    Initial Commit

---
+++ src/main/java/com/b2c/qa/base/Basefile.java
@@ -0,0 +1,10 @@
+package com.b2c.qa.base;
+
+public class Basefile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/b2c/qa/pages/Dashboardnew.java
@@ -0,0 +1,5 @@
+package com.b2c.qa.pages;
+
+public class Dashboardnew {
+
+}
